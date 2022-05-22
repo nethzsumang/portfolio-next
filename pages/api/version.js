f7270d327063132fd64a9eb76a0dbb019b12a6ba
Kenneth Sumang <kennethsumang08@gmail.com>
2022-05-22 23:53:23 +0800

    fix: remove copyright logo

--- pages/api/version.js
+++ pages/api/version.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   res.status(200).json({
-    version: '1.0.1',
-    since: '2022-05-18 16:05:00 UTC+0800',
+    version: '1.0.2',
+    since: '2022-05-22 23:51:00 UTC+0800',
     author: 'Kenneth Sumang',
     email: 'dev@kennethsumang.com'
   });
