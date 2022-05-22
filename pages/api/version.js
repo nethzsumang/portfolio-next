2d26759a23769cb43dc72f05e5b7d69a2c6b3b58
Kenneth Sumang <kennethsumang08@gmail.com>
2022-05-23 00:16:12 +0800

    fix: resume spelling error

--- pages/api/version.js
+++ pages/api/version.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   res.status(200).json({
-    version: '1.0.2',
-    since: '2022-05-22 23:51:00 UTC+0800',
+    version: '1.0.3',
+    since: '2022-05-23 00:15:00 UTC+0800',
     author: 'Kenneth Sumang',
     email: 'dev@kennethsumang.com'
   });
