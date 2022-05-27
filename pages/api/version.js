4b6e68d2cfb893b20febda763c6ca87c6f7a5754
Kenneth Sumang <kennethsumang08@gmail.com>
2022-05-28 00:32:35 +0800

    feat: update email addresses

--- pages/api/version.js
+++ pages/api/version.js
@@ -1,9 +1,9 @@
 export default function handler(req, res) {
   res.status(200).json({
-    version: '1.0.3',
-    since: '2022-05-23 00:15:00 UTC+0800',
+    version: '1.0.4',
+    since: '2022-05-28 00:32:00 UTC+0800',
     author: 'Kenneth Sumang',
-    email: 'dev@kennethsumang.com'
+    email: 'kdsumang@outlook.com'
   });
 }
   
