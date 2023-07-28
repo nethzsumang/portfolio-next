39c6ffc838deae758daa680b5dd3f911a8a8f4cd
Kenneth Sumang <kennethsumang08@gmail.com>
2023-07-28 11:02:40 +0800

    refactor: rename existing library

--- plugins/react-i18n.js
+++ plugins/react-i18n.js
@@ -1,6 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LangLibrary from '../libraries/LangLibrary';
+import LangLibrary from '../libraries/lang.library';
 
 i18n
   .use(initReactI18next)
